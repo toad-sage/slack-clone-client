c50ef2779ce06a7ed76d75483a0efd0b8a236a47
ritz-786 <ritikraj7255@gmail.com>
2020-05-19 10:43:44 +0530

    added infinte scroll

--- src/containers/MessageContainer.js
+++ src/containers/MessageContainer.js
@@ -69,6 +69,7 @@ class MessageContainer extends Component {
 		this.state = {
 			hasMoreItems: true
 		}
+		this.scroller = React.createRef();
 	}
 	
 	// eslint-disable-next-line react/no-deprecated
@@ -79,13 +80,30 @@ class MessageContainer extends Component {
 
 	// this is subscribe channel whenever channel is changed
 	//and channelId is recieved as props
-	componentWillReceiveProps({ channelId }) {
+	componentWillReceiveProps({ messages,channelId }) {
 		if(this.props.channelId !== channelId){
 			if(this.unsubscribe){
 				this.unsubscribe();
 			}
 			this.unsubscribe = this.subscribe(channelId);
 		}
+
+		if (
+	      this.scroller.current &&
+	      this.scroller.current.scrollTop < 20 &&
+	      this.props.data.messages &&
+	      messages &&
+	      this.props.data.messages.length !== messages.length
+	    ) {
+	      //20 items
+	      const heightBeforeRender = this.scroller.current.scrollHeight;
+	      // wait for 40 item to render
+	      setTimeout(() => {
+	        if (this.scroller.current) {
+	          this.scroller.current.scrollTop = this.scroller.current.scrollHeight - heightBeforeRender;
+	        }
+	      }, 200);
+	    }
 	}
 
 	componentWillUnmount(){
@@ -117,36 +135,43 @@ class MessageContainer extends Component {
 			},
 		});
 
+	handleScroll = () => {
+		const { hasMoreItems } = this.state;
+		const {data: {messages,fetchMore},channelId} = this.props
+		// console.log(this.scroller.current.scrollTop);
+		/* 20 is message limit on backend */
+		if(this.scroller && this.scroller.current.scrollTop < 100 && hasMoreItems && messages.length >=20){
+			fetchMore({
+				variables: {
+					channelId,
+					cursor: messages[messages.length -1].createdAt
+		        },
+		        updateQuery: (prev, { fetchMoreResult }) => {
+		            if (!fetchMoreResult) return prev;
+		            if(fetchMoreResult.messages.length === 0)
+		            {
+		            	this.setState({hasMoreItems: false})
+		            }
+		            return {
+		            	...prev,
+		            	messages: [...prev.messages,...fetchMoreResult.messages],
+		            }
+		        }
+			});
+		}
+	}
+
    render(){
 	// eslint-disable-next-line react/prop-types
 	const {data: {loading,messages,fetchMore},channelId} = this.props
 	return (loading ? null : (
-		<Messages>
-		<FileUpload channelId={channelId} disableClick>
+		<Messages 
+			onScroll={this.handleScroll}
+			ref = {this.scroller}>
+		<FileUpload style={{ display: 'flex', flexDirection: 'column-reverse' }} channelId={channelId} disableClick>
 				<Comment.Group>
-					{/* 20 is message limit on backend */}
-					{this.state.hasMoreItems && messages.length>=20 && (<Button onClick={() => {
-											fetchMore({
-												variables: {
-													channelId,
-													cursor: messages[messages.length -1].createdAt
-										        },
-										        updateQuery: (prev, { fetchMoreResult }) => {
-										            if (!fetchMoreResult) return prev;
-										            if(fetchMoreResult.messages.length === 0)
-										            {
-										            	this.setState({hasMoreItems: false})
-										            }
-										            return {
-										            	...prev,
-										            	messages: [...prev.messages,...fetchMoreResult.messages],
-										            }
-										        }
-											});
-										}}>Load More
-										</Button>)}
-					{messages.slice().reverse().map(m => (
-					<Comment key={`${m.id}-message`}>
+					{messages.slice().reverse().map((m,index) => (
+					<Comment key={`${index}-message`}>
 						<Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/matt.jpg' />
 						<Comment.Content>
 						<Comment.Author as='a'>{m.user.username}</Comment.Author>
